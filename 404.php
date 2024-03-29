551382c11bc8d10e2c94fc9dc0342c26ebee7a6d
Zhang <sunman_one@163.com>
2024-03-30 01:13:15 +0800

    style

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 ?>
 <html <?php language_attributes(); ?>>
 
-<head> 
+<head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php echo akina_option('favicon_link', ''); ?>" />
